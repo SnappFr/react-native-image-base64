4c82c88f236c5aca9543f713ab64e66d44557abe
Michaël Villeneuve <contact@michaelvilleneuve.fr>
2018-03-28 17:14:15 +0200

    Update package name

--- android/src/main/java/fr/snapp/imagebase64/RNImgToBase64Module.java
+++ android/src/main/java/fr/snapp/imagebase64/RNImgToBase64Module.java
@@ -1,5 +1,5 @@
 
-package com.reactlibrary;
+package fr.snapp.imagebase64;
 
 import android.graphics.Bitmap;
 import android.net.Uri;
--- android/src/main/java/fr/snapp/imagebase64/RNImgToBase64Package.java
+++ android/src/main/java/fr/snapp/imagebase64/RNImgToBase64Package.java
@@ -1,5 +1,5 @@
 
-package com.reactlibrary;
+package fr.snapp.imagebase64;
 
 import java.util.Arrays;
 import java.util.Collections;
